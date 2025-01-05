81d2a39beeb428fd44558fdf3a2084b082746050
Maddi Sai Lahari <119775037+Lahari03msl@users.noreply.github.com>
2025-01-05 22:52:57 +0530

    Delete electronics directory

--- electronics/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,get_object_or_404, redirect
-from django.http import HttpResponse
-from .models import Gadgets
-
-def display(request):
-    vals = Gadgets.objects.all()
-    return render(request,'test1.html',{'i':vals})
-
-# Create your views here.
