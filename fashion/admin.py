1f77a659211636b6754451b216ff9901d99eb777
Maddi Sai Lahari <119775037+Lahari03msl@users.noreply.github.com>
2025-01-05 22:53:19 +0530

    Delete fashion directory

--- fashion/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Trends
-
-# Register your models here.
-@admin.register(Trends)
-class TrendsAdmin(admin.ModelAdmin):
-    list_display = ('Style','colors','number_of_pieces')
--- fashion/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,get_object_or_404, redirect
-from django.http import HttpResponse
-from .models import Trends
-
-def display(request):
-    vals = Trends.objects.all()
-    return render(request,'test2.html',{'i':vals})
-
-# Create your views here.
